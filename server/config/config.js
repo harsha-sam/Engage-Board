c64940d7d3cf5195f1ccb89b30ce10a66eaec0c2
Harshavardhan <harsha.sam23@gmail.com>
2022-05-07 00:13:41 +0530

    Migrate db to heroku

--- server/config/config.js
+++ server/config/config.js
@@ -11,7 +11,7 @@ module.exports = {
     "dialectOptions": {
       "ssl": {
         "require": true,
-        rejectUnauthorized: false
+        "rejectUnauthorized": false
       }
     }
   },
@@ -35,6 +35,7 @@ module.exports = {
     "dialectOptions": {
       "ssl": {
         "require": true,
+        "rejectUnauthorized": false
       }
     }
   }
